6db70fb8e3a5690c60d994c4d636406b78267638
deepak-kumar-dev <deepakkumarlamoria@gmail.com>
2020-06-24 19:55:41 +0530

    Updates

---
+++ precache-manifest.7312d245f7e3f2a8adecd5583bbec3f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fcd6e208f7b8ec8458d3deb9f63f41",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "93de097bba8d6c701b20",
+    "url": "/profile/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "db3ff79d6458ee577987",
+    "url": "/profile/static/js/2.595ca213.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/profile/static/js/2.595ca213.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93de097bba8d6c701b20",
+    "url": "/profile/static/js/main.c896fb9e.chunk.js"
+  },
+  {
+    "revision": "b5a7d259590e62b51bcc",
+    "url": "/profile/static/js/runtime-main.3026efc4.js"
+  },
+  {
+    "revision": "3a367863f24244652f645d8e3ab6d168",
+    "url": "/profile/static/media/Resume_deepak.3a367863.PDF"
+  },
+  {
+    "revision": "64b6569251685e06b9560ce7d72bd776",
+    "url": "/profile/static/media/profile.64b65692.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.934ab97300b7e7cff6bbd9ca381ca6ea.js"
+  "/profile/precache-manifest.7312d245f7e3f2a8adecd5583bbec3f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
