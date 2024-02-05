543585ec77deabe5d24720acc8c6494a5565d6ee
Deepak <deepakkumarlamoria@gmail.com>
2024-02-05 21:25:57 +0530

    started code structure

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import AboutCard from "./src/Components/AboutCard";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="main">
+      <AboutCard></AboutCard>
+    </div>
+  );
 }
 
 const react = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/Components/AboutCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AboutCard() {
+  return (
+    <div className="about-card">
+      <h1 className="text-zinc-700 text-center">About Me</h1>
+      <p>
+        Full-stack Software Engineer developing web apps with the help of
+        javascript and its frameworks. Try to learn something new through every
+        single opportunity. Passionate about building end-to-end accessible,
+        high-performance, secure, scalable web applications. Keeping technical
+        skills aside, I love to travel and bike Riding.
+      </p>
+    </div>
+  );
+}
+
+export default AboutCard;
